459df64bd427f88879acd789475b6e935f499393
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2012-12-17 13:29:04 +0100

    added possibility to import/export FIM resources to/from XML

---
+++ src/FimCommunication.Tests/Import/XmlImporterTests.cs
@@ -0,0 +1,75 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.ResourceManagement.ObjectModel;
+using Predica.FimCommunication.Import;
+using Xunit;
+
+namespace Predica.FimCommunication.Tests.Import
+{
+    public class XmlImporterTests
+    {
+        private XmlImporter _importer;
+
+        public XmlImporterTests()
+        {
+            _importer = new XmlImporter();
+        }
+
+        private Stream OpenStream()
+        {
+            string resourceName = "Predica.FimCommunication.Tests.Import.exported-users.xml";
+
+            return Assembly.GetExecutingAssembly()
+                .GetManifestResourceStream(resourceName);
+        }
+
+        private ImportResult PerformImport()
+        {
+            using (var stream = OpenStream())
+            {
+                return _importer.Import(stream);
+            }
+        }
+
+        [Fact]
+        public void imports_exported_users()
+        {
+            var result = PerformImport();
+
+            Assert.Equal(5, result.PrimaryImportObjects.Count());
+
+            Assert.Equal("[x [x [x", result.PrimaryImportObjects.First()
+                .Attributes.Single(x => x.Key.Name == "City")
+                .Value.Value);
+            Assert.Equal("Administrator", result.PrimaryImportObjects
+                .Last().Attributes.Single(x => x.Key.Name == "AccountName")
+                .Value.Value);
+        }
+
+        [Fact]
+        public void detects_references()
+        {
+            var user = PerformImport().PrimaryImportObjects.First();
+
+            Assert.IsType<RmReference>(user["Creator"].Value);
+        }
+
+        [Fact]
+        public void imports_all_exported_objects_including_references()
+        {
+            var result = PerformImport();
+
+            Assert.Equal(8, result.AllImportedObjects.Count());
+
+            Assert.Equal("Person", result.AllImportedObjects.Skip(0).First().ObjectType);
+            Assert.Equal("DomainConfiguration", result.AllImportedObjects.Skip(1).First().ObjectType);
+            Assert.Equal("qfOuAssignement", result.AllImportedObjects.Skip(2).First().ObjectType);
+            Assert.Equal("Person", result.AllImportedObjects.Skip(3).First().ObjectType);
+            Assert.Equal("Person", result.AllImportedObjects.Skip(4).First().ObjectType);
+            Assert.Equal("qfOuAssignement", result.AllImportedObjects.Skip(5).First().ObjectType);
+            Assert.Equal("Person", result.AllImportedObjects.Skip(6).First().ObjectType);
+            Assert.Equal("Person", result.AllImportedObjects.Skip(7).First().ObjectType);
+        }
+    }
+}
---
+++ src/FimCommunication/Export/XmlExporter.cs
@@ -0,0 +1,47 @@
+﻿using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Microsoft.ResourceManagement.Automation;
+using Microsoft.ResourceManagement.Automation.ObjectModel;
+using NLog;
+
+namespace Predica.FimCommunication.Export
+{
+    public interface IXmlExporter
+    {
+        void WriteXml(Stream stream, string xpath);
+    }
+
+    public class XmlExporter : IXmlExporter
+    {
+        /// <summary>
+        /// Uses logic from ConvertFrom-FIMResource cmdlet to fetch export objects
+        /// and xml-serializes them to a given stream
+        /// </summary>
+        public void WriteXml(Stream stream, string xpath)
+        {
+            _log.Debug("Fetching export objects for query {0}", xpath);
+
+            string url = ConfigurationManager.AppSettings["fimServiceBaseUrl"];
+
+            var exportConfig = new ExportConfig
+            {
+                CustomConfig = new[] { xpath },
+                Uri = url,
+            };
+
+            var results = exportConfig.Invoke<ExportObject>()
+                .ToList();
+
+            _log.Debug("Fetched {0} export objects for query {1}", results.Count, xpath);
+
+            // code copied from ConvertFrom-FIMResource cmdlet
+            var root = new XmlRootAttribute("Results");
+            var serializer = new XmlSerializer(typeof(ExportObject[]), root);
+            serializer.Serialize(stream, results.ToArray());
+        }
+
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger();
+    }
+}
